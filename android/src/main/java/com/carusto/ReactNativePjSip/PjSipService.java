5d7e7ca7c5894f8e9f431834b4a2d40ed7431eb7
Francisco C <francisco.caraballo@gmail.com>
2021-03-08 19:28:56 +0100

    WIP: show full screen intent notification on call received

--- android/src/main/java/com/carusto/ReactNativePjSip/PjSipService.java
+++ android/src/main/java/com/carusto/ReactNativePjSip/PjSipService.java
@@ -4,6 +4,7 @@ import android.app.Service;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -968,7 +969,30 @@ public class PjSipService extends Service {
 
             return;
         }
+        String ns = getApplicationContext().getPackageName();
+        String cls = ns + ".MainActivity";
+        Intent fullScreenIntent = new Intent(getApplicationContext(), Class.forName(cls));
+        // Intent fullScreenIntent = new Intent(this, CallActivity.class);
+        PendingIntent fullScreenPendingIntent = PendingIntent.getActivity(getApplicationContext(), 0,
+        fullScreenIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
+        NotificationCompat.Builder notificationBuilder =
+            new NotificationCompat.Builder(this, "main")
+            .setSmallIcon(R.drawable.ic_foreground_notification)
+            .setContentTitle("Incoming call")
+            .setContentText("(919) 555-1234")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setCategory(NotificationCompat.CATEGORY_CALL)
+
+        // Use a full-screen intent only for the highest-priority alerts where you
+        // have an associated activity that you would like to launch after the user
+        // interacts with the notification. Also, if your app targets Android 10
+        // or higher, you need to request the USE_FULL_SCREEN_INTENT permission in
+        // order for the platform to invoke this notification.
+        .setFullScreenIntent(fullScreenPendingIntent, true);
+
+        Notification incomingCallNotification = notificationBuilder.build();
+        startForeground(2, incomingCallNotification);
         /**
         // Automatically start application when incoming call received.
         if (mAppHidden) {
